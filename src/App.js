46e853ae9aa2a8ede14075e7ed2f80a009124fb2
Seles <selemtei@gmail.com>
2022-05-31 21:43:19 +0300

    finale coming soon

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import './App.css';
+import React from 'react';
+import CountdownTimer from './CountdownTimer';
 
 function App() {
+  const SIX_DAYS_IN_MS = 6 * 24 * 60 * 60 * 1000;
+  const NOW_IN_MS = new Date().getTime();
+  const dateTimeAfterSixDays = NOW_IN_MS + SIX_DAYS_IN_MS;
+
   return (
     <div className="background-image">
       <div className="top-left">
-        <h3>MAKENIS Inc</h3>
-        <p>A Corporation involved in the technology and travel sector</p>
+        <h3>MAKENIS LIMITED</h3>
+        <p>A company involved in the technology and travel sector</p>
       </div>
       <div className="middle">
         <h1>COMING SOON</h1>
-        <hr />
-        <p>12 days left</p>
+        <CountdownTimer targetDate={dateTimeAfterSixDays} />
       </div>
     </div>
   );
